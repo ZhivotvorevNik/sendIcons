aac4f906e1aee663074a8331b634adca0468ffe2
Nikolay Zhivotvorev <zhivotvorev.nik@gmail.com>
2016-03-19 00:04:23 +0300

    кнопка загрузки

--- app/static/assets/app.js
+++ app/static/assets/app.js
@@ -4,4 +4,8 @@ $(function(){
         sevrices.removeClass('service_shown_yes');
         $('.service_type_' + this.value).addClass('service_shown_yes');
     })
+
+    $('.services__load-input').on('change', function(){
+        $(this).parents('form').submit();
+    })
 })
